e8e3c5b51c2bb05a2d92dd187b30ad51867bc2fc
pyhas <42034008+pyhas@users.noreply.github.com>
2022-03-31 23:15:33 -0400

    ignoring unconfigured APNs 2

--- test_epg_conn.py
+++ test_epg_conn.py
@@ -1,5 +1,5 @@
 from netmiko import ConnectHandler
-import test_elements
+import elements
 from datetime import date, datetime
 from influxdb import InfluxDBClient
 from rich import print
@@ -22,50 +22,53 @@ def getapnstats(elem):
         password="Eric@2020",
     )
     for apn in elem['apns']:
-        my_dict = {}
-        tags = {}
-        tags['apn_name'] = apn
-        tags['epg'] = elem['hostname']
-        command = f'epg pgw apn {apn} statistics'
-        sfp = net_connect.send_command(command, use_ttp=True, ttp_template="/home/system/scripts/m2m/ttp_template/test_statstics.ttp")
-        for d in sfp[0]:
-            fields = {}
-            my_dict['measurement'] = measure
-            my_dict['tags'] = tags
-            for t in d['traffic']:
+        try:
+            my_dict = {}
+            tags = {}
+            tags['apn_name'] = apn
+            tags['epg'] = elem['hostname']
+            command = f'epg pgw apn {apn} statistics'
+            sfp = net_connect.send_command(command, use_ttp=True, ttp_template="/home/system/scripts/m2m/ttp_template/test_statstics.ttp")
+            for d in sfp[0]:
+                fields = {}
+                my_dict['measurement'] = measure
+                my_dict['tags'] = tags
+                for t in d['traffic']:
+                    try:
+                        fields['downlink'] = int(t['downlink'])
+                    except:
+                        pass
+                    try:
+                        fields['uplink'] = int(t['uplink'])
+                    except:
+                        pass
+                fields['pdp'] = int(d['pdp']['pdp_active'])
+                fields['pdn'] = int(d['pdp']['pdn'])
+                fields['ue'] = int(d['pdp']['ue_count'])
+                fields['gx-ccr-termination-failed'] = 0
+                fields['gx-ccr-update-failed'] = 0
+                fields['gx-ccr-initial-failed'] = 0
+                fields['gy-ccr-termination-failed'] = 0
+                fields['gy-ccr-update-failed'] = 0
+                fields['gy-ccr-initial-failed'] = 0
                 try:
-                    fields['downlink'] = int(t['downlink'])
+                    for x in d['gx']:
+                        fields['gx-ccr-termination-failed'] += int(x['ccr-termination-failed'])
+                        fields['gx-ccr-update-failed'] += int(x['ccr-update-failed'])
+                        fields['gx-ccr-initial-failed'] += int(x['ccr-initial-failed'])
                 except:
                     pass
                 try:
-                    fields['uplink'] = int(t['uplink'])
+                    for y in d['gy']:
+                        fields['gy-ccr-termination-failed'] += int(y['ccr-termination-failed'])
+                        fields['gy-ccr-update-failed'] += int(y['ccr-update-failed'])
+                        fields['gy-ccr-initial-failed'] += int(y['ccr-initial-failed'])
                 except:
                     pass
-            fields['pdp'] = int(d['pdp']['pdp_active'])
-            fields['pdn'] = int(d['pdp']['pdn'])
-            fields['ue'] = int(d['pdp']['ue_count'])
-            fields['gx-ccr-termination-failed'] = 0
-            fields['gx-ccr-update-failed'] = 0
-            fields['gx-ccr-initial-failed'] = 0
-            fields['gy-ccr-termination-failed'] = 0
-            fields['gy-ccr-update-failed'] = 0
-            fields['gy-ccr-initial-failed'] = 0
-            try:
-                for x in d['gx']:
-                    fields['gx-ccr-termination-failed'] += int(x['ccr-termination-failed'])
-                    fields['gx-ccr-update-failed'] += int(x['ccr-update-failed'])
-                    fields['gx-ccr-initial-failed'] += int(x['ccr-initial-failed'])
-            except:
-                pass
-            try:
-                for y in d['gy']:
-                    fields['gy-ccr-termination-failed'] += int(y['ccr-termination-failed'])
-                    fields['gy-ccr-update-failed'] += int(y['ccr-update-failed'])
-                    fields['gy-ccr-initial-failed'] += int(y['ccr-initial-failed'])
-            except:
-                pass
-            my_dict['fields'] = fields
-            apn_stastics.append(my_dict)
+                my_dict['fields'] = fields
+                apn_stastics.append(my_dict)
+        except Exception as xx:
+            pass
     net_connect.disconnect()
 
 with ThreadPoolExecutor(max_workers=8) as executor:

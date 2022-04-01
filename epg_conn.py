60e92078de00b1ce233af055323e4c5313deb86b
pyhas <42034008+pyhas@users.noreply.github.com>
2022-03-31 22:37:31 -0400

    ignore unconfigured APNs

--- epg_conn.py
+++ epg_conn.py
@@ -23,42 +23,45 @@ def getapnstats(elem):
         password="Eric@2020",
     )
     for apn in elem['apns']:
-        my_dict = {}
-        tags = {}
-        tags['apn_name'] = apn
-        tags['epg'] = elem['hostname']
-        command = f'epg pgw apn {apn} statistics'
-        sfp = net_connect.send_command(command, use_ttp=True, ttp_template="/home/system/scripts/m2m/ttp_template/statstics.ttp")
-        # print(sfp)
-        for d in sfp[0]:
-            down = 0
-            up = 0
-            try:
-                fields = {}
-                my_dict['measurement'] = measure
-                my_dict['tags'] = tags
-                for t in d['traffic']:
-                    down = down + int(t['downlink'])
-                    up = up + int(t['uplink'])
-                fields['downlink'] = down
-                fields['uplink'] = up
-                fields['pdp'] = d['pdp']['pdp_active']
-                fields['pdn'] = d['pdp']['pdn']
-                fields['ue'] = d['pdp']['ue_count']
-                my_dict['fields'] = fields
-                apn_stastics1.append(my_dict)
-            except (KeyError, TypeError):
-                tags['apn_name'] = apn
-                tags['epg'] = elem['hostname']
-                my_dict['measurement'] = measure
-                my_dict['tags'] = tags
-                fields['downlink'] = 0
-                fields['uplink'] = 0
-                fields['pdp'] = d['pdp']['pdp_active']
-                fields['pdn'] = d['pdp']['pdn']
-                fields['ue'] = d['pdp']['ue_count']
-                my_dict['fields'] = fields
-                apn_stastics1.append(my_dict)
+        try:
+            my_dict = {}
+            tags = {}
+            tags['apn_name'] = apn
+            tags['epg'] = elem['hostname']
+            command = f'epg pgw apn {apn} statistics'
+            sfp = net_connect.send_command(command, use_ttp=True, ttp_template="/home/system/scripts/m2m/ttp_template/statstics.ttp")
+            # print(sfp)
+            for d in sfp[0]:
+                down = 0
+                up = 0
+                try:
+                    fields = {}
+                    my_dict['measurement'] = measure
+                    my_dict['tags'] = tags
+                    for t in d['traffic']:
+                        down = down + int(t['downlink'])
+                        up = up + int(t['uplink'])
+                    fields['downlink'] = down
+                    fields['uplink'] = up
+                    fields['pdp'] = d['pdp']['pdp_active']
+                    fields['pdn'] = d['pdp']['pdn']
+                    fields['ue'] = d['pdp']['ue_count']
+                    my_dict['fields'] = fields
+                    apn_stastics1.append(my_dict)
+                except (KeyError, TypeError):
+                    tags['apn_name'] = apn
+                    tags['epg'] = elem['hostname']
+                    my_dict['measurement'] = measure
+                    my_dict['tags'] = tags
+                    fields['downlink'] = 0
+                    fields['uplink'] = 0
+                    fields['pdp'] = d['pdp']['pdp_active']
+                    fields['pdn'] = d['pdp']['pdn']
+                    fields['ue'] = d['pdp']['ue_count']
+                    my_dict['fields'] = fields
+                    apn_stastics1.append(my_dict)
+        except Exception as xx:
+            pass
     net_connect.disconnect()
 
 

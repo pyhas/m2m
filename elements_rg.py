d165bb3138f01b07a97119d9898c2abbc15d010f
pyhas <42034008+pyhas@users.noreply.github.com>
2022-06-11 20:08:31 -0400

    initial free rg

---
+++ elements_rg.py
@@ -0,0 +1,77 @@
+elements = [
+  {
+    "hostname": "BAS-EPG01",
+    "type": "ggsn",
+    "ip": "10.128.90.212",
+    "apns": [
+      "ego",
+      "internet"
+    ]
+  },
+  {
+    "hostname": "BISCex-EPG01",
+    "type": "ggsn",
+    "ip": "10.137.8.193",
+    "apns": [
+       "ego",
+      "internet"
+    ]
+  },
+  {
+    "hostname": "CAMPex-EPG01",
+    "type": "ggsn",
+    "ip": "10.137.40.4",
+    "apns": [
+      "A2Z",
+       "ego",
+      "internet"
+    ]
+  },
+  {
+    "hostname": "KIR-EPG01",
+    "type": "ggsn",
+    "ip": "10.129.33.244",
+    "apns": [
+      "ego",
+      "internet"
+    ]
+  },
+  {
+    "hostname": "ERB-EPG01",
+    "type": "ggsn",
+    "ip": "172.21.253.140",
+    "apns": [
+      "ego",
+      "internet"
+    ]
+  },
+   {
+    "hostname": "RAM-EPG01",
+    "type": "ggsn",
+    "ip": "172.20.94.244",
+    "apns": [
+      "ego",
+      "extension",
+      "internet"
+    ]
+  },
+  {
+    "hostname": "NAJex-EPG01",
+    "type": "ggsn",
+    "ip": "10.128.140.193",
+    "apns": [
+      "ego",
+      "internet"
+    ]
+  },
+  {
+    "hostname": "DIWex-EPG01",
+    "type": "ggsn",
+    "ip": "10.137.163.2",
+    "apns": [
+      "ego",
+      "internet"
+    ]
+  }
+]
+
